a032de303861f71b5f223b04f8bdcf6f557b7129
Anthony Walker <anthony.walker.j@gmail.com>
2020-04-14 19:41:00 -0400

    Added httpoison and poison to dependencies Updated init with more details and more abstract email Maid frontend post call more verbose Implemented backend functionality to deliver payment

--- backend/lib/backend_web/controllers/deliver_controller.ex
+++ backend/lib/backend_web/controllers/deliver_controller.ex
@@ -12,7 +12,34 @@ defmodule BackendWeb.DeliverController do
     end
 
     def create(%Plug.Conn{body_params: body_params} = conn, _body_and_query_params) do
+        payment_method_token = body_params["payment_method_token"]
+
+        receiver_token = "YOUR_RECEIVER_TOKEN_HERE"
+        url = "https://core.spreedly.com/v1/receivers/#{receiver_token}/deliver.json"
+        environment_key = "YOUR_ENVIRONMENT_KEY_HERE"
         Logger.debug "Token: #{body_params[:token]}"
+        environment_secret = "YOUR_ENVIRONMENT_SECRET_HERE"
+        authentication = "#{environment_key}:#{environment_secret}"
+
+        body = %{
+            delivery: %{
+                payment_method_token: payment_method_token,
+                url: "https://spreedly-echo.herokuapp.com",
+                headers: "Content-Type: application/json",
+                body: Poison.encode!(%{
+                        card_number: "{{ credit_card_number }}"
+                })
+            }
+        }
+
+        body = Poison.encode(body) |> elem(1)
+
+        headers = ["Authorization": "Basic #{Base.encode64(authentication)}==", "Accept": "application/json; Charset=utf-8", "Content-Type": "application/json"]
+        options = [ssl: [{:versions, [:'tlsv1.2']}], recv_timeout: 500]
+        {:ok, response} = HTTPoison.post(url, body, headers, options)
+
+        IO.puts "RESPONSE========================================"
+        IO.inspect response
 
         json conn, nil
     end  
--- backend/mix.exs
+++ backend/mix.exs
@@ -20,7 +20,7 @@ defmodule Backend.MixProject do
   def application do
     [
       mod: {Backend.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :httpoison, :poison]
     ]
   end
 
@@ -42,7 +42,9 @@ defmodule Backend.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:httpoison, "~> 1.6"},
+      {:poison, "~> 3.1"}
     ]
   end
 
